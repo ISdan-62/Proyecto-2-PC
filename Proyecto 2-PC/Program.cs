a809829d89c9e83c486c039fba1f0ab7e201c5eb
erick <erick@LEGI0SORIO>
2026-04-30 18:52:29 -0600

    Se creo el codigo para contraseña donde se comprueba la contrasena correcta y su validacion en signos de asteriscos

--- Proyecto 2-PC/Program.cs
+++ Proyecto 2-PC/Program.cs
@@ -22,3 +22,65 @@
     }
 }
 string resultadoUsuario = Vadiacion1();
+
+string Validacion2()
+{
+    string contrasenaCorrecta = "+Ma0An+PC";
+    string ingreso;
+
+    do
+    {
+        Console.WriteLine();
+        Console.Write("Ingrese la contraseña: ");
+        ingreso = LeerPassword();
+
+        if (ingreso != contrasenaCorrecta)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Contraseña Invalida");
+            Console.WriteLine("Presione cualquier tecla para volver a intentar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------Contraseña valida-------");
+        }
+
+    } while (ingreso != contrasenaCorrecta);
+
+    return ingreso;
+}
+string resultadoContrasena = Validacion2();
+
+static string LeerPassword()
+{
+    string pass = "";
+    ConsoleKeyInfo tecla;
+
+    while (true)
+    {
+        tecla = Console.ReadKey(true);
+
+        if (tecla.Key == ConsoleKey.Enter)
+        {
+            break;
+        }
+        else if (tecla.Key == ConsoleKey.Backspace)
+        {
+            if (pass.Length > 0)
+            {
+                pass = pass.Substring(0, pass.Length - 1);
+                Console.Write("\b \b");
+            }
+        }
+        else
+        {
+            pass += tecla.KeyChar;
+            Console.Write("*");
+        }
+    }
+    return pass;
+}
+
