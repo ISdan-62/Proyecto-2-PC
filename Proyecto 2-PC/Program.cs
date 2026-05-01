bd7de14d31b6ffa670c332b4817247c8ae31c8aa
erick <erick@LEGI0SORIO>
2026-04-30 18:48:09 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto 2-PC/Program.cs
@@ -0,0 +1,24 @@
+﻿string Vadiacion1()
+{
+    string usuario;
+
+    while (true)
+    {
+        Console.Write("Ingrese el usuario: ");
+        usuario = Console.ReadLine();
+        if (usuario == "JTello")
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------Usuario valido-------");
+            return usuario;
+        }
+        else
+        {
+            Console.WriteLine("Uasio inválida");
+            Console.WriteLine("Presione cualquier tecla para volver a intentar");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
+string resultadoUsuario = Vadiacion1();
